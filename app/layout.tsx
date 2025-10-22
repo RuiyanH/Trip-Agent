7b0ff8461c9b69c8c17986165e258ca90c9ab549
Ruiyan Huang <54792448+RuiyanH@users.noreply.github.com>
2025-10-21 21:56:50 -0400

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen bg-white text-neutral-900">{children}</body>
-    </html>
-  );
-}
